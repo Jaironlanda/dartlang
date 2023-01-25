edb1feaeca4d789ed8c40f4e67a41a713996e144
Jairon Landa <jaironlanda@yahoo.com>
2023-01-25 20:49:03 +0800

    first commit

---
+++ bin/basicMath.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  int a = 1;
+  int b = 2;
+
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+}
---
+++ bin/forloop.dart
@@ -0,0 +1,6 @@
+  void main() {
+    const iterable = ['Salad', 'Popcorn', 'Toast'];
+    for (final element in iterable) {
+      print(element);
+    }
+  }
